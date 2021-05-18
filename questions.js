327a39c833f70dd95388372b747f3370d5b20d61
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 12:27:56 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"___ your car blue?",
+        options:["Do", "Is", "Are", "Am"],
+        answer: 1
+    },
+    {
+     q:"___ Alice from London?",
+        options:["Is", "Are", "Does", "Am"],
+        answer: 0   
+    },
+    {
+        q:"___ I late?",
+        options:["Are", "Is", "Do", "Am"],
+        answer: 3
+    },
+    {
+        q:"___ you hungry?",
+        options:["Is", "-", "Do", "Are"],
+        answer: 3
+    },
+    {
+        q:"___ the shops open today?",
+        options:["-", "Is", "Are", "Do"],
+        answer: 2
+    },
+    {
+        q:"___ they at home",
+        options:["Do", "Are", "-", "Is"],
+        answer: 1
+    },
+    {
+        q:"___ your hands cold?",
+        options:["Are", "-", "Do", "Is"],
+        answer: 0
+    },
+    {
+        q:"___ your mother at work?",
+        options:["Are", "Does", "-", "Is"],
+        answer: 3
+    },
+    {
+        q:"How old ___ you?",
+        options:["-", "are", "is", "do"],
+        answer: 1
+    },
+    {
+        q:"What ___ your favourite colors?",
+        options:["is", "do", "are", "-"],
+        answer: 2
+    }
+
+]
